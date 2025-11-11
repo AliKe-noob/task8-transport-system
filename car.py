8e4f8e5eb7b388ccf887b37e2ee5621248e3cf43
AliKe <kenesbekovalmuhammed7@gmail.com>
2025-11-12 01:03:34 +0500

    Py doesn't allow default values come before required ones, fixed.

--- car.py
+++ car.py
@@ -1,7 +1,7 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    def __init__(self, brand, model, year, current_speed=0, max_speed=None, num_doors, fuel_type):
+    def __init__(self, brand, model, year,  num_doors, fuel_type, current_speed=0, max_speed=None):
         super().__init__(brand, model, year, current_speed, max_speed)
         self.num_doors = num_doors
         self.fuel_type = fuel_type
