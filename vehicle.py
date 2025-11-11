af88c2bd9a97788ca052f2e120ea010d2dcfe86e
AliKe <kenesbekovalmuhammed7@gmail.com>
2025-11-11 20:46:04 +0500

    Added max_speed for better realism, needs fixing acc, brake.

--- vehicle.py
+++ vehicle.py
@@ -1,9 +1,10 @@
 class Vehicle:
-    def __init__(self, brand, model, year, current_speed = 0):
+    def __init__(self, brand, model, year, current_speed = 0, max_speed = None):
         self.brand = brand
         self.model = model
         self.year = year
         self.current_speed = current_speed
+        self.max_speed = max_speed
 
     def start(self):
         print("The engine started.")
@@ -30,4 +31,8 @@ class Vehicle:
 
 
     def info(self, speed):
-        print("")
+        print(f"""
+Brand = {self.brand}
+Model = {self.model}
+
+              """)
